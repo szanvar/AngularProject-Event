7c2a2ef979bc7294ca6bd90920fe377b25e07bb2
seemazanvar <srzanvar_dcd@jspm.edu.in>
2021-08-17 15:16:01 +0530

    First Angular Event Project

---
+++ src/app/demo/demo.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demo',
+  template: `<button (click)="fun()"> Hello </button>
+  {{name}}
+  <label>MarvellousLabel</label>
+  <input type="text" name="" id="">`
+})
+export class DemoComponent implements OnInit 
+{
+  name :string = "";
+  // name : string | undefined
+  fun()
+  {
+    this.name = "Button Clicked";
+    //console.log("Button Clicked");
+    
+  }
+  constructor() 
+  {
+    //this.name = "";
+  }
+
+  ngOnInit(): void {
+  }
+
+}
